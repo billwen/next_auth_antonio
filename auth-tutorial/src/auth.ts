afb3b6330823eced618202aa5c14c39232747f59
Gang Wen <73705196+gawen-ikea@users.noreply.github.com>
2024-02-21 21:49:28 +0100

    2:23

--- auth-tutorial/src/auth.ts
+++ auth-tutorial/src/auth.ts
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth";
-import GitHub from "@auth/core/providers/github";
+import authConfig from "@/auth.config";
+import {db} from "@/lib/db";
+import {PrismaAdapter} from "@auth/prisma-adapter";
 
 export const {
   handlers: {GET, POST},
   auth
 } = NextAuth({
-  providers: [GitHub]
+  adapter: PrismaAdapter(db),
+  session: {strategy: "jwt"},
+  ...authConfig
 });
--- auth-tutorial/src/middleware.ts
+++ auth-tutorial/src/middleware.ts
@@ -1,8 +1,12 @@
-import {auth} from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+const {auth} = NextAuth(authConfig);
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
   console.log("Route: ", req.nextUrl.pathname);
+  console.log("Is logged in: ", isLoggedIn);
 });
 
 // Optional, don't invoke the middleware on some paths
