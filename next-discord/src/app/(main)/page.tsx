9fea4f5b596730c91d49c4084063039d238d1ebb
Gang Wen <73705196+gawen-ikea@users.noreply.github.com>
2024-03-09 16:27:06 +0100

    Added Clerk support

--- next-discord/src/app/(main)/page.tsx
+++ next-discord/src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
+      <UserButton afterSignOutUrl="/" />
       <Button>Click me</Button>
     </main>
   );
